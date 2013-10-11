5a5f6c7d8b210c1d9d51f1cf205123a9214d5b2b
Lauro Neto <lauro.neto@openbossa.org>
2013-10-10 21:02:43 -0300

    Simple code to get the hash of the files.
    TODO: Make it persistent in some kind of db.

--- finddup/finddup.py
+++ finddup/finddup.py
@@ -6,21 +6,43 @@
 import os
 import sys
 import hashlib
+from collections import defaultdict
+
+import re
+
+IMAGE_REGEX = re.compile("JPG$|CR2$", re.IGNORECASE)
+
+digests = defaultdict(list)
+
+def hash_file(filename):
+    with open(filename, 'rb') as handle:
+        hasher = hashlib.md5()
+
+        data = handle.read(128)
+        while data:
+            hasher.update(data)
+            data = handle.read(128)
+
+        return hasher.digest()
 
 def find_duplicates(directory):
     filenumber = 0
     dir_number = 0
     for root, dirs, files in os.walk(directory):
-        print "%s with #dirs: %s and #files: %s" % (root, len(dirs),
-                                                    len(files))
-
         already_counted_directory = False
 
         for filename in files:
-            filename = filename.lower()
-
-            if filename.endswith("jpg") or filename.endswith("cr2"):
+            if IMAGE_REGEX.search(filename):
                 filenumber += 1
+                print filename
+
+                digest = hash_file(os.path.join(root, filename))
+
+                dirname = os.path.dirname(root)
+
+                if digest in digests:
+                    "Oops, file %s already found" % filename
+                digests[digest].append((filename, dirname))
 
                 if not already_counted_directory:
                     dir_number += 1
